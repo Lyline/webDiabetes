c680ffb09134b18dd7dbd42b649257ea42ed965d
Lyline <greg.quesne@gmail.com>
2022-05-19 09:45:53 +0200

    Impl - Home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {PatientListComponent} from "./patientList/patientList.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent},
+  { path: "patients", component: PatientListComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
